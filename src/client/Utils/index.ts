8077fa650a3700644d3f2c579b8ee2481c36febe
Kim Jae Hoon <hoon.development@gmail.com>
2022-06-14 15:53:16 +0900

    feat: getRpcUrl 함수 추가

--- src/client/Utils/index.ts
+++ src/client/Utils/index.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 import {SolanaConfig} from "./solanaConfig";
 
+/**
+ * 솔라나 컨피그 객체 가져오는 함수
+ */
 export async function getConfig(): Promise<SolanaConfig> {
   const CONFIG_FILE_PATH = path.resolve(
     os.homedir(),
@@ -20,4 +23,11 @@ export async function getConfig(): Promise<SolanaConfig> {
   }
   catch(e) {}
   return solanaConfig;
+}
+
+/**
+ * 설정된 솔라나 RPC URL 반환 해주는 함수
+ */
+export async function getRpcUrl(): Promise<string> {
+  return (await getConfig()).get("json_rpc_url");
 }
