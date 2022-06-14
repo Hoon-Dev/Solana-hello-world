ff20f403015a6a33ae78ade9583d642408741f5b
Kim Jae Hoon <hoon.development@gmail.com>
2022-06-14 15:37:52 +0900

    feat: 유틸 함수 모음 추가

---
+++ src/client/Utils/index.ts
@@ -0,0 +1,20 @@
+import os from "os";
+import fs from "fs";
+import path from "path";
+
+import {SolanaConfig} from "./solanaConfig";
+
+export async function getConfig(): Promise<SolanaConfig> {
+  const CONFIG_FILE_PATH = path.resolve(
+    os.homedir(),
+    ".config",
+    "solana",
+    "cli",
+    "config.yml",
+  );
+
+  const solanaConfig = SolanaConfig.getInstance();
+  const configContent = await fs.promises.readFile(CONFIG_FILE_PATH, {encoding: "utf-8"});
+  solanaConfig.set(configContent);
+  return solanaConfig;
+}
